9b3326428d213e38dc594153c69f3c9461bd3fa5
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2024-02-21 16:30:35 +0300

    Added image videos.

--- src/test/java/AnnotatedSentence/StatisticsTest.java
+++ src/test/java/AnnotatedSentence/StatisticsTest.java
@@ -1,9 +1,81 @@
 package AnnotatedSentence;
+import org.junit.Before;
+import org.junit.Test;
+
+import Dictionary.Pos;
+import WordNet.*;
 
 import java.io.File;
+import java.util.HashSet;
 import java.util.Map;
 
 public class StatisticsTest {
+
+    @Test
+    public void verbsMultiFolder(){
+        WordNet turkish = new WordNet();
+        String dataset = "FrameNet-Examples";
+        File[] listOfFiles = new File("../../FrameNet-Examples/Turkish-Phrase/").listFiles();
+        for (File file:listOfFiles) {
+            if (file.isDirectory()){
+                String fileName = file.getName();
+                AnnotatedCorpus corpus = new AnnotatedCorpus(new File("../../" + dataset + "/Turkish-Phrase/" + fileName));
+                for (int i = 0; i < corpus.sentenceCount(); i++){
+                    AnnotatedSentence sentence = (AnnotatedSentence) corpus.getSentence(i);
+                    HashSet<String> verbs = new HashSet<>();
+                    for (int j = 0; j < sentence.wordCount(); j++){
+                        AnnotatedWord word = (AnnotatedWord) sentence.getWord(j);
+                        if (word.getSemantic() != null){
+                            SynSet synSet = turkish.getSynSetWithId(word.getSemantic());
+                            if (synSet != null && synSet.getPos().equals(Pos.ADVERB)){
+                                verbs.add(word.getSemantic());
+                            }
+                        }
+                    }
+                    for (String id: verbs){
+                        System.out.print(id + "\t" + turkish.getSynSetWithId(id).getDefinition() + "\t" + dataset + "-" + fileName + "\t" + sentence.getFileName() + "\t" +  sentence.toWords() + "\t");
+                        for (int j = 0; j < sentence.wordCount(); j++){
+                            AnnotatedWord word = (AnnotatedWord) sentence.getWord(j);
+                            if (word.getSemantic() != null && word.getSemantic().equals(id)){
+                                System.out.print(" " + word.getName());
+                            }
+                        }
+                        System.out.println();
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    public void nouns(){
+        WordNet turkish = new WordNet();
+        String dataset = "Penn-Treebank-20";
+        AnnotatedCorpus corpus = new AnnotatedCorpus(new File("../../" + dataset + "/Turkish-Phrase/"));
+        for (int i = 0; i < corpus.sentenceCount(); i++){
+            AnnotatedSentence sentence = (AnnotatedSentence) corpus.getSentence(i);
+            HashSet<String> verbs = new HashSet<>();
+            for (int j = 0; j < sentence.wordCount(); j++){
+                AnnotatedWord word = (AnnotatedWord) sentence.getWord(j);
+                if (word.getSemantic() != null){
+                    SynSet synSet = turkish.getSynSetWithId(word.getSemantic());
+                    if (synSet != null && synSet.getPos().equals(Pos.ADVERB)){
+                        verbs.add(word.getSemantic());
+                    }
+                }
+            }
+            for (String id: verbs){
+                System.out.print(id + "\t" + turkish.getSynSetWithId(id).getDefinition() + "\t" + dataset + "\t" + sentence.getFileName() + "\t" +  sentence.toWords() + "\t");
+                for (int j = 0; j < sentence.wordCount(); j++){
+                    AnnotatedWord word = (AnnotatedWord) sentence.getWord(j);
+                    if (word.getSemantic() != null && word.getSemantic().equals(id)){
+                        System.out.print(" " + word.getName());
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
 
     public void statistics(){
         AnnotatedCorpus corpus = new AnnotatedCorpus(new File("../../Penn-Treebank-20/Turkish-Phrase/"));
